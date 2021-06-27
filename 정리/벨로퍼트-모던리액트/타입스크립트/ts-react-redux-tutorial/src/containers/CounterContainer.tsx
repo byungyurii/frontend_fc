bce9a55f92443d59e439dd0708f62b57fcf0872d
Byun, Gyuri <byungyuri126@gmail.com>
2021-06-27 19:59:04 +0900

    add: week11 github clone

--- 정리/벨로퍼트-모던리액트/타입스크립트/ts-react-redux-tutorial/src/containers/CounterContainer.tsx
+++ 정리/벨로퍼트-모던리액트/타입스크립트/ts-react-redux-tutorial/src/containers/CounterContainer.tsx
@@ -7,9 +7,9 @@ import Counter from '../components/Counter';
 export interface CounterContainerProps {}
 
 const CounterContainer = (props: CounterContainerProps) => {
-  // 상태를 조회합니다. 상태를 조회 할 때에는 state 의 타입을 RootState 로 지정해야합니다.
+
   const count = useSelector((state: RootState) => state.counter.count);
-  const dispatch = useDispatch(); // 디스패치 함수를 가져옵니다
+  const dispatch = useDispatch(); 
 
   const onIncrease = () => {
     dispatch(increase());
